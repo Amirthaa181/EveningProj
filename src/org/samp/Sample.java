14529b37918bd6b595722fc9a88f9b85d0b3b4ec
Amirthaa181 <samirtha79@gmail.com>
2021-02-16 19:20:55 +0530

    Person 2 added some lines

--- src/org/samp/Sample.java
+++ src/org/samp/Sample.java
@@ -4,6 +4,8 @@ public class Sample {
 	
 	public static void main(String[] args) {
 		
+		System.out.println("Person 2 is adding his code");
+		System.out.println("Person 2 done his work ");
 	}
 
 }
